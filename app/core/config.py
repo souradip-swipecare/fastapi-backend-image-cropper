d29693a954c33c3003e0b0b7012476f9f05e65ad
Souradip Biswas <souradipbiswas@Souradips-MacBook-Air.local>
2026-01-22 22:59:45 +0530

    update ne feature

--- app/core/config.py
+++ app/core/config.py
@@ -13,10 +13,23 @@ def initialize_firebase():
         if firebase_admin._apps:
             return  
         print("firebase loaded started")   
-        cred = credentials.Certificate("souradip-opencv-assignment-serviceAccountKey.json")
+        # cred = credentials.Certificate("souradip-opencv-assignment-serviceAccountKey.json")
+        cred = credentials.Certificate({
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+})
         firebase_admin.initialize_app(cred,{
         "storageBucket": "souradip-opencv-assignment.appspot.com"
-    })
+        })
 
         print("firebase loaded")
         return
